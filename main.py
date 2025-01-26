c4a939dee9f55134076f8143e0122bbd5f7b4f55
bf4r <sharpslpcz@gmail.com>
2025-01-26 12:21:55 +0100

    add ai command and requirements

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
 import discord
 import os
+from openai import OpenAI
 
+# config
+COMMAND_PREFIX = "."
+AI_BASE_URL = "https://openrouter.ai/api/v1/"
+AI_MODEL = "openai/gpt-4o-mini" # adjust the name based on the API, for example in the OpenAI API it would be gpt-4o-mini, this one is for OpenRouter
+
+# setup
 bot_token = os.environ["MYLA_BOT_TOKEN"]
+ai_api_key = os.environ["AI_API_KEY"]
 
-PREFIX = "."
+ai_client = OpenAI(
+    api_key=ai_api_key,
+    base_url=AI_BASE_URL
+)
 
-def iscmd(message, cmdstr):
-    return message.content.startswith(PREFIX + cmdstr);
+def iscmd(msg, cmdstr):
+    return msg.content.startswith(COMMAND_PREFIX + cmdstr);
+
+async def ask_ai(msg):
+    prompt = msg.content[3:].strip() # remove ".ai "
+    completion = ai_client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": prompt,
+            }
+        ],
+        model=AI_MODEL,
+    )
+    return completion.choices[0].message.content
 
 class MyClient(discord.Client):
     async def on_ready(self):
@@ -18,6 +42,10 @@ class MyClient(discord.Client):
             await msg.reply("hello")
         elif iscmd(msg, "ping"):
             await msg.reply("pong!")
+        elif iscmd(msg, "ai"):
+            response_text = await ask_ai(msg)
+            await msg.reply(response_text)
+
 
 intents = discord.Intents.default()
 intents.message_content = True
