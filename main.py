e688e98a3b5772c37416aa311de7f29c0333f8bd
bf4r <sharpslpcz@gmail.com>
2025-01-26 12:07:10 +0100

    add iscmd function to make things easier

--- main.py
+++ main.py
@@ -5,14 +5,17 @@ bot_token = os.environ["MYLA_BOT_TOKEN"]
 
 PREFIX = "."
 
+def iscmd(message, cmdstr):
+    return message.content.startswith(PREFIX + cmdstr);
+
 class MyClient(discord.Client):
     async def on_ready(self):
         print(f"Logged on as {self.user}!")
 
-    async def on_message(self, message):
-        print(f"Message from {message.author}: {message.content}")
-        if message.content.startswith(PREFIX + "hey"):
-            await message.channel.send("hello")
+    async def on_message(self, msg):
+        print(f"{msg.author}: {msg.content}")
+        if iscmd(msg, "hey"):
+            await msg.reply("hello")
 
 intents = discord.Intents.default()
 intents.message_content = True
