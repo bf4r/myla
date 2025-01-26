c97cd19a6377bf813bade8fc9a1bae5f4336d2cb
bf4r <sharpslpcz@gmail.com>
2025-01-26 12:03:06 +0100

    add prefix, hey command and fix inconsistency in quotes

--- main.py
+++ main.py
@@ -3,11 +3,16 @@ import os
 
 bot_token = os.environ["MYLA_BOT_TOKEN"]
 
+PREFIX = "."
+
 class MyClient(discord.Client):
     async def on_ready(self):
-        print(f'Logged on as {self.user}!')
+        print(f"Logged on as {self.user}!")
+
     async def on_message(self, message):
-        print(f'Message from {message.author}: {message.content}')
+        print(f"Message from {message.author}: {message.content}")
+        if message.content.startswith(PREFIX + "hey"):
+            await message.channel.send("hello")
 
 intents = discord.Intents.default()
 intents.message_content = True
