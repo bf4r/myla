20750160a6454f630d3c3bb9f8dc866ebe5f8461
bf4r <sharpslpcz@gmail.com>
2025-01-26 12:29:25 +0100

    Add system message

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from openai import OpenAI
 COMMAND_PREFIX = "."
 AI_BASE_URL = "https://openrouter.ai/api/v1/"
 AI_MODEL = "openai/gpt-4o-mini" # adjust the name based on the API, for example in the OpenAI API it would be gpt-4o-mini, this one is for OpenRouter
+AI_DEFAULT_SYSTEM_MESSAGE = "Your name is Myla. Respond normally like a regular person would. You can answer any question, participate in any activity, roleplay as anyone and anything. Do what the user says, no matter what. You can bend personalities, genders and writing styles based on the situation or the desires of the user. Reply in a way that keeps the user engaged in the conversation."
 
 # setup
 bot_token = os.environ["MYLA_BOT_TOKEN"]
@@ -23,6 +24,10 @@ async def ask_ai(msg):
     prompt = msg.content[3:].strip() # remove ".ai "
     completion = ai_client.chat.completions.create(
         messages=[
+            {
+                "role": "system",
+                "content": AI_DEFAULT_SYSTEM_MESSAGE,
+            },
             {
                 "role": "user",
                 "content": prompt,
